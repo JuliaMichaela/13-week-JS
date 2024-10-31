043b9cccb9c7da885cbbbd15925e81f490d1aeea
JuliaMichaela <juliaperez@yandex.ru>
2024-10-31 23:47:25 +0300

    created function countDays

---
+++ app.js
@@ -0,0 +1,13 @@
+const birthday = document.querySelector('#birthday');
+
+function countDays () {
+    console.log (birthday.value);
+    console.log (1)
+}
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', countDays )
+
+
+
