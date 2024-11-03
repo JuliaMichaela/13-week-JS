e11f1517a06c06d1d25c7b04670387f9101e0a04
JuliaMichaela <juliaperez@yandex.ru>
2024-11-03 21:51:53 +0300

    finished task

--- app.js
+++ app.js
@@ -7,23 +7,35 @@ function calculateDays () {
 
     if (birthday === '') {
         error.textContent = 'Пожалуйста, введите дату рождения'
-        error.classList.toggle('error');
+        error.classList.add('error');
     } else {
         error.textContent = '';
-        error.classList.toggle('error');
+        error.classList.remove('error');
+
+        let date = Date.parse(birthday);
+        let currentDate = new Date().getTime();
+        let days = Math.ceil((date - currentDate)/(1000*60*60*24));
+
+        let strDay;
+        if (days === 1){
+            strDay = 'день'
+        } else if (days>=2 && days<=4){
+            strDay = 'дня'
+        } else if(days.toString().slice(-1) === '1'){
+            strDay = 'день'
+        } else if (days.toString().slice(-1) === '2' || days.toString().slice(-1) === '3' || days.toString().slice(-1) === '4'){
+            strDay = 'дня'
+        } else {
+            strDay = 'дней'
+        }
+
+        result.textContent= `До Вашего дня рождения осталось ${days} ${strDay} `
+        return days;
     }
-    let date = Date.parse(birthday);
-    let currentDate = new Date().getTime();
-    let days = Math.ceil((date - currentDate)/(1000*60*60*24));
-    let strDay= 'день';
-    result.textContent= `До Вашего дня рождения осталось ${days}  ${strDay} `
-    return days;
-    
 }
     
-
 const btn = document.querySelector('#btn');
-btn.addEventListener('click', calculateDays )
+btn.addEventListener('click', calculateDays)
 
 
 
